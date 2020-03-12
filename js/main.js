14ff5b510965955e978db13c50e80233ebadd9fc
Aayush Jain <jain.aay@husky.neu.edu>
2020-03-11 23:42:53 -0400

    Fixed Bugs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let formulaMap = new Map();
+// let spreadsheet.formulaMap = new Map();
 let cells_set = [];
 let input = document.getElementById('spreadsheet_1');
 
@@ -30,7 +30,6 @@ let source = Rx.Observable.fromEvent(input, 'keyup')
                 // to check if the formula cell is not a part of the formula
                 let flag = true;
 
-
                 if (result_cell_col >= start_col && result_cell_col <= end_col) {
                     if (result_cell_row >= start_row && result_cell_row <= end_row) {
                         flag = false;
@@ -47,7 +46,8 @@ let source = Rx.Observable.fromEvent(input, 'keyup')
                     // console.log(`${start_col} ${start_row} ${end_col} ${end_row}`);
                     // console.log(`${no_of_rows} ${no_of_cols}`);
                     // console.log("---------");
-                    formulaMap.set(result_cell, formula);
+                    //spreadsheet.formulaMap.set("inp-"+result_cell, formula);
+                    spreadsheet.formulaMap.set(e.target.id, e.target.value);
                     let sum = 0;
                     for (let i = start_col.charCodeAt(0); i <= end_col.charCodeAt(0); i++) {
                         let chr = String.fromCharCode(i);
@@ -76,9 +76,7 @@ let source = Rx.Observable.fromEvent(input, 'keyup')
         }
     })
 
-// var code = Rx.Observable.from([
-//     "Enter"
-// ]);
+
 let temp = Rx.Observable.fromEvent(input, 'keyup')
     .map((e) => e)
     .filter(e => e.target.value.startsWith('='))
@@ -103,21 +101,114 @@ let temp = Rx.Observable.fromEvent(input, 'keyup')
                 s = s.replace(parts[i], value);
 
             }
-            console.log(s);
+            //console.log(s);
             let total = math.evaluate(s);
-            // let total = 0;
-            // s = s.match(/[+\-]*(\.\d+|\d+(\.\d+)?)/g) || [];
-            // while (s.length) {
-            //     let val = document.getElementById(s.shift());
-            //     let value = 0;
-            //     if (/^\+?(0|[1-9]\d*)$/.test(val))
-            //         value = parseFloat(val);
-            //     total += parseFloat(s.shift());
-            // }
-            console.log(total);
-            if (!isNaN(total))
+            //console.log(total);
+            if (!isNaN(total)) {
+                spreadsheet.formulaMap.set(e.target.id, e.target.value);
                 e.target.value = total;
+            }
+
         }
 
 
+    })
+
+
+let updateFields = Rx.Observable.fromEvent(input, 'keyup')
+    .map((e) => e)
+    .subscribe(e => {
+        if (e.code == "Tab") {
+            for (let [k, v] of spreadsheet.formulaMap) {
+                let input_cell = document.getElementById(k);
+                if (v.startsWith('=SUM(')) {
+
+                    //extracting formula data and metadata
+                    let formula = v;                                                 // '=SUM(B1:D9)'
+                    let result_cell = input_cell.parentNode.id;                                    //'B10'
+                    let result_cell_col = result_cell.substring(0, 1);                         //'B'
+                    let result_cell_row = parseInt(result_cell.substring(1, result_cell.length));       //10
+
+                    //extracting column metadata mentioned in the formula by the user
+                    let start_cell = formula.split('(').pop().split(':')[0];    //B1
+                    let end_cell = formula.split(':').pop().split(')')[0];      //D9
+                    let start_col = start_cell.substring(0, 1);                 //B
+                    let start_row = parseInt(start_cell.substring(1, start_cell.length)); //1
+                    let end_col = end_cell.substring(0, 1);                     //D
+                    let end_row = parseInt(end_cell.substring(1, end_cell.length));      //9
+
+                    // extracting spreadsheet details
+                    let no_of_rows = input.rows.length - 1;
+                    let no_of_cols = input.rows[1].querySelectorAll("td").length;
+
+                    // to check if the formula cell is not a part of the formula
+                    let flag = true;
+
+                    if (result_cell_col >= start_col && result_cell_col <= end_col) {
+                        if (result_cell_row >= start_row && result_cell_row <= end_row) {
+                            flag = false;
+                        }
+                    }
+
+                    //check if the cells entered in the formula are valid cells in the spreadsheet
+                    if (start_col >= 'A' && start_col <= String.fromCharCode(64 + no_of_cols)
+                        && end_col >= 'A' && end_col <= String.fromCharCode(64 + no_of_cols)
+                        && start_row >= 1 && start_row <= no_of_rows
+                        && end_row >= 1 && end_row <= no_of_rows
+                        && flag) {
+
+                        // console.log(`${start_col} ${start_row} ${end_col} ${end_row}`);
+                        // console.log(`${no_of_rows} ${no_of_cols}`);
+                        // console.log("---------");
+                        //spreadsheet.formulaMap.set("inp-"+result_cell, formula);
+                        let sum = 0;
+                        for (let i = start_col.charCodeAt(0); i <= end_col.charCodeAt(0); i++) {
+                            let chr = String.fromCharCode(i);
+                            for (let j = start_row; j <= end_row; j++) {
+
+                                //console.log(`${i} and ${j}`);
+                                let val = document.getElementById("inp-" + chr + j).value;
+                                let value = 0;
+                                if (/^\+?(0|[1-9]\d*)$/.test(val))
+                                    value = parseInt(val);
+                                sum += value;
+
+                            }
+                        }
+                        input_cell.value = sum;
+
+                    } else {
+
+                        e.target.value = "INVALID!";
+    
+                    }
+
+                }
+                else {
+                    let s_orig = v;
+                    let s = s_orig.substring(1, s_orig.length);
+                    let parts = s.split(/[^A-Za-z0-9]/);
+
+                    for (i = 0; i < parts.length; i++) {
+                        let val = 0, value = 0;
+                        let node = document.getElementById("inp-" + parts[i]);
+                        if (node !== null) {
+                            val = node.value;
+                        }
+                        if (/^\+?(0|[1-9]\d*)$/.test(val)) {
+                            value = parseInt(val);
+                        }
+                        value = value.toString();
+                        s = s.replace(parts[i], value);
+
+                    }
+                    //console.log(s);
+                    let total = math.evaluate(s);
+                    //console.log(total);
+                    if (!isNaN(total)) {
+                        input_cell.value = total;
+                    }
+                }
+            }
+        }
     })
--- js/view.js
+++ js/view.js
@@ -2,6 +2,7 @@ class Spreadsheet {
     constructor() {
         this.selectedRow = null;
         this.selectedColumn = null;
+        this.formulaMap = new Map();
 
         //creates a table
         let spreadsheet = document.createElement("table");
@@ -54,12 +55,11 @@ class Spreadsheet {
     }
 
     addColumn(){
-
         //check if rows and columns are less than 25
         let input = document.getElementById('spreadsheet_1');
-        let no_of_rows = input.rows.length - 1;
+        let no_of_rows = input.rows.length;
         let no_of_cols = input.rows[1].querySelectorAll("td").length;
-        if(no_of_cols <= 25){
+        if(no_of_cols <= 25 && this.selectedColumn > 0){
             
             //create column header for the new column
             let column_header = document.createElement("th");
@@ -79,65 +79,197 @@ class Spreadsheet {
                     ele.setAttribute('type', 'text');
                     ele.setAttribute('value', '');
                     cell.appendChild(ele);
-                    row.appendChild(cell);
-                
+                    input.rows[i].appendChild(cell);                
             }
+
+            for(let p = no_of_cols; p >= this.selectedColumn+1; p--){
+                this.moveColData(p, p+1, no_of_rows);
+            }
+
+            for(let k = 1; k < no_of_rows; k++){
+                let kth_row_input_cols = spreadsheet_1.rows[k].querySelectorAll("input");
+                kth_row_input_cols[this.selectedColumn].value = " ";
+            }
+
+
         }else{
-            alert("Cannot add more than 26 Columns");
+            alert("Number of columns cannot exceed 26 or the selection is invalid");
             return;
         }
 
-        setSelectedRowsColumnsAsNull();
+        this.setSelectedRowsColumnsAsNull();
     }
 
     addRow(){
+<<<<<<< HEAD
         //check if rows and columns are less than 25
         setSelectedRowsColumnsAsNull();
+=======
+        //console.log(this.selectedRow);
+        //check if rows and columns are less than 25
+        let input = document.getElementById('spreadsheet_1');
+        let no_of_rows = input.rows.length - 1;
+        let no_of_cols = input.rows[1].querySelectorAll("td").length;
+        if(no_of_rows <= 25 && this.selectedRow > 0){
+            
+            //create row header for the new row
+            let row = document.createElement("tr");
+            row.id = "row-" + input.rows.length;
+            
+
+            //create row header
+            let row_header = document.createElement("th");
+            row_header.setAttribute("scope", "row");
+            row_header.id = "row-" + input.rows.length;
+            row_header.className = "row-header-class";
+            row_header.innerText = input.rows.length;
+            row.appendChild(row_header);
+            
+            // create one more column for every row
+            for(let i = 1; i <= no_of_cols; i++) {                  
+                    let cell = document.createElement("td");
+                    let chr = String.fromCharCode(64 + i);
+                    cell.id = chr + input.rows.length;
+                    let ele = document.createElement('input');
+                    ele.id="inp-"+cell.id;
+                    ele.setAttribute('type', 'text');
+                    ele.setAttribute('value', '');
+                    cell.appendChild(ele);
+                    row.appendChild(cell);
+                
+            }
+            input.appendChild(row);
+
+            //now that the row has been added, the data needs to be shifted 
+            for(let p = input.rows.length-1; p > this.selectedRow +1 ; p--){
+                this.moveRowData(spreadsheet_1.rows[p-1], spreadsheet_1.rows[p]);
+            } 
+
+            let cols = spreadsheet_1.rows[this.selectedRow +1].querySelectorAll("input");
+            for(let i = 0; i < cols.length; i++){
+                //console.log(cols[i].value);
+                cols[i].value=" ";
+            }
+
+        }else{
+            alert("Number of rows are already 26 or the selection is invalid");
+            return;
+        }
+
+        this.setSelectedRowsColumnsAsNull();
+>>>>>>> 2c71540... Fixed Bugs
     }
 
-    moveDataOnDeletion(){
-        
+
+    //transfer col 1 data into col 2
+    moveColData(col1_index, col2_index, no_of_rows){
+        let spreadsheet_1 = document.getElementById("spreadsheet_1");
+        for(let i = 1; i< no_of_rows; i++){
+            let ith_row_input_cols = spreadsheet_1.rows[i].querySelectorAll("input");
+            ith_row_input_cols[col2_index-1].value = ith_row_input_cols[col1_index-1].value;
+        }
+    }
+
+    //transfer row 1 data into row 2
+    moveRowData(row1, row2){
+        let cells_row1 = row1.querySelectorAll("input");
+        let cells_row2 = row2.querySelectorAll("input");
+        for(let i=0; i<cells_row1.length; i++){
+            cells_row2[i].value = cells_row1[i].value;
+        }
     }
 
     delRow(){
+        //console.log(this.selectedRow);
         let spreadsheet_1 = document.getElementById("spreadsheet_1");
-        let columns_of_first_row = spreadsheet_1.rows[1].getElementsByTagName("td");
-        if(this.selectedRow !== null && spreadsheet_1.rows.length > 2 && columns_of_first_row.length >1){
-            spreadsheet_1.deleteRow(this.selectedRow);
+        if(this.selectedRow > 0 && spreadsheet_1.rows.length > 2){   
+            
+            //to remove the formula from the map if it is in the row going to get deleted now
+            for (let [k, v] of this.formulaMap){
+                if(spreadsheet_1.rows[this.selectedRow].querySelector("#"+ k)!==null){
+                    this.formulaMap.delete(k);
+                }
+            }     
+
+            for(let i = this.selectedRow; i < spreadsheet_1.rows.length - 1; i++){                
+                this.moveRowData(spreadsheet_1.rows[i+1], spreadsheet_1.rows[i]);
+            }     
+            spreadsheet_1.deleteRow(spreadsheet_1.rows.length - 1);
             this.setSelectedRowsColumnsAsNull();
         }else{
-            alert("Selection Invalid!");
+            alert("Number of rows cannot fall below 1 or the selection is invalid");
         }         
     }
 
     delColumn(){       
         let spreadsheet_1 = document.getElementById("spreadsheet_1");
-        let columns_of_first_row = spreadsheet_1.rows[1].getElementsByTagName("td");
-        if(this.selectedColumn !== null && columns_of_first_row.length >1){
-            spreadsheet_1.rows[0].deleteCell(this.selectedColumn);
+        let no_of_input_cells = spreadsheet_1.rows[1].getElementsByTagName("td").length;
+
+        //to remove the formula from the map if it is in the column going to get deleted now
+        if(this.selectedColumn > 0 && no_of_input_cells > 1){
             for(let i = 1; i < spreadsheet_1.rows.length; i++){
-                spreadsheet_1.rows[i].deleteCell(this.selectedColumn);
+                let ith_row_input_cols = spreadsheet_1.rows[i].querySelectorAll("td");
+                let selectedCol = ith_row_input_cols[this.selectedColumn-1];
+                for (let [k, v] of this.formulaMap){
+                    if(selectedCol.querySelector("#"+ k)!==null){
+                        this.formulaMap.delete(k);
+                    }
+                } 
+            }
+
+            for(let i = this.selectedColumn + 1; i <= no_of_input_cells; i++){
+                this.moveColData(i, i-1, spreadsheet_1.rows.length);
+            }
+            for(let i = 0; i < spreadsheet_1.rows.length; i++){                
+                spreadsheet_1.rows[i].deleteCell(no_of_input_cells);
             }
         }else{
-            alert("Selection Invalid!");
+            alert("Number of columns cannot fall below 1 or the selection is invalid");
         }
         this.setSelectedRowsColumnsAsNull();       
+    }
+
+    importCSV(input) {
+        let file = input.files[0];
+        var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+        if (regex.test(input.value.toLowerCase())) {
+            if (typeof (FileReader) != "undefined") {
+                var reader = new FileReader();
+                reader.readAsText(file);
+                reader.onload = function (e) {                    
+                    var rows = e.target.result.split("\n");
+                    //console.log(rows);
+                    for (var i = 0; i < rows.length; i++) {
+                        let htmlRowNumber = i+1;
+                        var cells = rows[i].split(",");
+                        //console.log(cells);
+                        if (cells.length > 0) {                            
+                            for (var j = 0; j < cells.length; j++) {
+                                let htmlColumnChar = String.fromCharCode(65 + j);
+                                let inputCell = "inp-" + htmlColumnChar + htmlRowNumber;                                
+                                document.getElementById(inputCell).value = cells[j];
+                            }
+                        }
+                    }
+                }               
+            } else {
+                alert("This browser does not support HTML5.");
+            }
+        } else {
+            alert("Please upload a valid CSV file.");
+        }
     }
 
     exportTableToCSV(filename) {
         var csv = [];
         var rows = document.querySelectorAll("table tr");
         
-        for (var i = 0; i < rows.length; i++) {
-            var row = [], cols = rows[i].querySelectorAll("td");
-            
+        for (var i = 1; i < rows.length; i++) {
+            var row = [], cols = rows[i].querySelectorAll("td");            
             for (var j = 0; j < cols.length; j++){
-                //var inputVal = document.getElementById("myInput").value;
                 var inputVal = cols[j].querySelector("input").value;
                 row.push(inputVal);
-            } 
-                
-            
+            }                            
             csv.push(row.join(","));        
         }
     
@@ -172,6 +304,13 @@ class Spreadsheet {
     }
 
     setSelectedRowsColumnsAsNull(){
+        let spreadsheet_1 = document.getElementById("spreadsheet_1");
+        let elems = spreadsheet_1.querySelectorAll(".selected");
+        for(let i = 0; i < elems.length; i++){
+            if(elems[i] !== null){
+                elems[i].classList.remove("selected");
+            }
+        }
         this.selectedColumn = null;
         this.selectedRow = null;
     }
@@ -183,7 +322,8 @@ let spreadsheet = new Spreadsheet();
 //Added Add Row button
 let x = document.createElement("button");
 x.classList.add("button");
-x.onclick = spreadsheet.addRow;
+//x.onclick = spreadsheet.addRow;
+x.addEventListener('click', function() {spreadsheet.addRow();}); 
 let y = document.createTextNode("Add Row");
 x.appendChild(y);
 document.body.insertBefore(x, document.getElementById("spreadsheet_1"));
@@ -200,7 +340,8 @@ document.body.insertBefore(p, document.getElementById("spreadsheet_1"));
 //Added Add Column button
 let m = document.createElement("button");
 m.classList.add("button");
-m.onclick = spreadsheet.addColumn;
+//m.onclick = spreadsheet.addColumn;
+m.addEventListener('click', function() {spreadsheet.addColumn();}); 
 let n = document.createTextNode("Add Column");
 m.appendChild(n);
 document.body.insertBefore(m, document.getElementById("spreadsheet_1"));
@@ -214,12 +355,18 @@ g.appendChild(h);
 document.body.insertBefore(g, document.getElementById("spreadsheet_1"));
 
 //Added Import Data button
-let e = document.createElement("button");
-e.classList.add("button");
-e.addEventListener('click', function() {spreadsheet.importCSV("assignment6_data.csv");}); 
-let f = document.createTextNode("Import CSV");
-e.appendChild(f);
+let e = document.createElement("input");
+e.setAttribute("type", "file");
+e.id = "fileUpload";
+e.classList.add("button", "inputfile");
+e.addEventListener('change', function() {spreadsheet.importCSV(this);}); 
+var w = document.createElement("label");
+w.classList.add("button");
+var l = document.createTextNode("Import CSV");
+w.setAttribute("for", "fileUpload");
+w.appendChild(l);
 document.body.insertBefore(e, document.getElementById("spreadsheet_1"));
+document.body.insertBefore(w, document.getElementById("spreadsheet_1"));
 
 //Added Export Data button
 let c = document.createElement("button");
@@ -229,21 +376,16 @@ let d = document.createTextNode("Export as CSV");
 c.appendChild(d);
 document.body.insertBefore(c, document.getElementById("spreadsheet_1"));
 
+
 document.getElementById("spreadsheet_1").addEventListener("click", (event) => {
 
-    // to remove the selection
-    let spreadsheet_1 = document.getElementById("spreadsheet_1");
-    let elems = spreadsheet_1.querySelectorAll(".selected");
-    for(let i = 0; i < elems.length; i++){
-        if(elems[i] !== null){
-            elems[i].classList.remove("selected");
-        }
-    }
+    // to remove the existing selection
     spreadsheet.setSelectedRowsColumnsAsNull();
 
     // to select the column
     if(event.target.classList.contains('column-header-class')){
         spreadsheet.selectedColumn = event.target.cellIndex;
+        //console.log(spreadsheet.selectedColumn);
         for(let i = 1; i < spreadsheet_1.rows.length; i++){
             let ith_row = spreadsheet_1.rows[i];
             let sel_col = ith_row.getElementsByTagName("td")[spreadsheet.selectedColumn-1];
@@ -253,6 +395,7 @@ document.getElementById("spreadsheet_1").addEventListener("click", (event) => {
     // to select the row
     } else if(event.target.classList.contains('row-header-class')){
         spreadsheet.selectedRow = event.target.parentNode.rowIndex;
+        //console.log(spreadsheet.selectedRow);
         document.getElementById(event.target.parentNode.id).classList.add("selected");
     }
 
